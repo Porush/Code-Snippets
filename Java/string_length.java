f03cee77f37702c2ea54186c40c7fc5f05e28322
Shubh-Raikwar <32975436+Shubh-Raikwar@users.noreply.github.com>
2022-10-02 19:53:06 +0530

    Create string_length.java

---
+++ Java/string_length.java
@@ -0,0 +1,5 @@
+public class Sample_String{
+  public static void main(String[] args){  //Our sample string for this tutorial
+  String str_Sample = "RockStar";
+  //Length of a String
+  System.out.println("Length of String: " + str_Sample.length());}}
